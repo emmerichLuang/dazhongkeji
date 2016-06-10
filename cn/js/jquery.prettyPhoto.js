5cb35dcdef746d9482219e1451b6b5724e522b3d
emmerich <emmerichliang@gmail.com>
2016-06-11 01:37:16 +0800

    部分提交

--- cn/js/jquery.prettyPhoto.js
+++ cn/js/jquery.prettyPhoto.js
@@ -16,8 +16,8 @@
 			autoplay_slideshow: false, /* true/false */
 			opacity: 0.30, /* Value between 0 and 1 */
 			show_title: true, /* true/false */
-			allow_resize: true, /* Resize the photos bigger than viewport. true/false */
-			allow_expand: true, /* Allow the user to expand a resized image. true/false */
+			allow_resize: false, /* Resize the photos bigger than viewport. true/false */
+			allow_expand: false, /* Allow the user to expand a resized image. true/false */
 			default_width:950,
 			default_height: 580,
 			counter_separator_label: '/', /* The separator for the gallery counter 1 "of" 2 */
@@ -75,7 +75,7 @@
 					<div class="pp_overlay" ></div>',
 			gallery_markup: '',
 			image_markup: '<img id="fullResImage" src="{path}" />',
-			flash_markup: '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="{width}" height="{height}"><param name="wmode" value="{wmode}" /><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="{path}" /><embed src="{path}" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="{width}" height="{height}" wmode="{wmode}"></embed></object>',
+			flash_markup: '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="{width}" height="{height}"><param name="wmode" value="{wmode}" /><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="never" /><param name="movie" value="{path}" /><embed src="{path}" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="never" width="{width}" height="{height}" wmode="{wmode}"></embed></object>',
 			quicktime_markup: '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" height="{height}" width="{width}"><param name="src" value="{path}"><param name="autoplay" value="{autoplay}"><param name="type" value="video/quicktime"><embed src="{path}" height="{height}" width="{width}" autoplay="{autoplay}" type="video/quicktime" pluginspage="http://www.apple.com/quicktime/download/"></embed></object>',
 			iframe_markup: '<iframe src ="{path}" width="{width}" height="{height}" frameborder="no"  SCROLLING="no" marginheight="0" marginwidth="0" ></iframe>',
 			inline_markup: '<div class="pp_inline">{content}</div>',
--- js/sywx.js
+++ js/sywx.js
@@ -21,7 +21,7 @@ lastScrollY=lastScrollY+percent;
 }
 suspendcode12="<DIV id=\"lovexin12\" style='left:2px;POSITION:absolute;TOP:119px; z-index: 9999;'></div>"
 
-suspendcode14="<DIV id=\"lovexin14\" style='right:2px;POSITION:absolute;TOP:119px; z-index: 9999;'><a href='#' target='_blank'><img border=0 src=images/sywx.jpg><br><a href=JavaScript:; onclick=\"lovexin14.style.display = 'none'\"><img border=0 src=images/close_dl.gif></div>"
+suspendcode14="<DIV id=\"lovexin14\" style='right:2px;POSITION:absolute;TOP:119px; z-index: 9999;'><a href='#' target='_blank'><img border=0 src=../images/sywx.jpg><br><a href=JavaScript:; onclick=\"lovexin14.style.display = 'none'\"><img border=0 src=../images/close_dl.gif></div>"
 
 document.write(suspendcode12); 
 document.write(suspendcode14); 
